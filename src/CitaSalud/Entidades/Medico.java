76b47c2e44c68ea5821b3f2c9c508e8062e79a8a
Andres Stefano <xcloudsgls@gmail.com>
2023-05-27 20:46:40 -0500

    Creación de la herencia con la superclase 'Persona' para médico y paciente

--- src/CitaSalud/Entidades/Medico.java
+++ src/CitaSalud/Entidades/Medico.java
@@ -1,81 +1,20 @@
 package CitaSalud.Entidades;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
  *
  * @author Andres
  */
-public class Medico {
-    private String dni;
-    private String nombre;
-    private String apellido;
-    private Date fechaNacimiento;
+public class Medico extends Persona {
     private String email;
     private List<Area> area;
     
     public Medico() {
         this.area = new ArrayList<>();
     }
-
-    /**
-     * @return the dni
-     */
-    public String getDni() {
-        return dni;
-    }
-
-    /**
-     * @param dni the dni to set
-     */
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the apellido
-     */
-    public String getApellido() {
-        return apellido;
-    }
-
-    /**
-     * @param apellido the apellido to set
-     */
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    /**
-     * @return the fechaNacimiento
-     */
-    public Date getFechaNacimiento() {
-        return fechaNacimiento;
-    }
-
-    /**
-     * @param fechaNacimiento the fechaNacimiento to set
-     */
-    public void setFechaNacimiento(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
-
+    
     /**
      * @return the email
      */
