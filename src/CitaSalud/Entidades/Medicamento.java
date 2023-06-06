9604fe3c6ecfbf2b024d892dec44f22d1a30ad66
Eusebio-Abad <eusebio9abad@gmail.com>
2023-06-07 00:50:57 +0200

    añadi la clase Medicamento

---
+++ src/CitaSalud/Entidades/Medicamento.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CitaSalud.Entidades;
+
+/**
+ *
+ * @author Eusebio
+ */
+public class Medicamento {
+    
+        private String nombre;
+      private String descripcion;
+      private int cantidad;
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+  
+}
