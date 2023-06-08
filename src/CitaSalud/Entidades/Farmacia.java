aa9e8a43064609191e05a9aab78d93ea25de7a2a
Eusebio-Abad <eusebio9abad@gmail.com>
2023-06-08 23:16:20 +0200

    Añadi la clase farmacia

---
+++ src/CitaSalud/Entidades/Farmacia.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CitaSalud.Entidades;
+
+/**
+ *
+ * @author USER
+ */
+public class Farmacia {
+    
+    private Paciente paciente;
+    private Consultorio consultorio;
+    
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public Consultorio getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(Consultorio consultorio) {
+        this.consultorio = consultorio;
+    }
+    
+}
